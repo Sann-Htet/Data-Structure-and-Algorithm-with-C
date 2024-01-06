f0fb40c1ef34bf860a129ce691c4e393b2994692
Sann-Htet <sannhtet899@gmail.com>
2024-01-07 03:58:51 +0630

    pointer introduction

--- Day-8/1-pointer.c
+++ Day-8/1-pointer.c
@@ -3,8 +3,11 @@
 int main(){
     int a = 10;
     int *ptr = &a;
-    printf("%d\n", a);
-    printf("%x", *ptr);
+    printf("A value: %d\n", a);
+    printf("A memory address: %x\n", ptr);
+    printf("Pointer value: %d\n", *ptr);
+    printf("Pointer memory address: %x\n", ptr);
+    printf("Pointer address: %x\n", &ptr);
 
     return 0;
 }
