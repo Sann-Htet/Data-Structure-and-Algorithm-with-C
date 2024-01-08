79b40c4747a4ac787772d96711e394d40474dc71
Sann-Htet <sannhtet899@gmail.com>
2024-01-08 14:00:03 +0630

    pass by reference

---
+++ Day-9/reference.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+
+void pass_value(int a, int b);
+
+int main(){
+    int a = 10;
+    int b = 20;
+    printf("This is main func!\n");
+    printf("Before function call : a=%d b=%d\n", a, b);
+    pass_value(a, b);
+    printf("After calling function!\n");
+    printf("Values a=%d b=%d\n", a, b);
+
+    return 0;
+}
+
+void pass_value(int a, int b){
+    // printf("%d %d\n", a, b);
+    int c = a + b;
+}
